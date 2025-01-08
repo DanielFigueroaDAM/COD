fe093cc5dd5ea0f5b7b5d9dfc6262870865c3adc
Figueroa <dfigueroavidal@danielcastelao.org>
2025-01-08 14:20:00 +0100

    esto es exp 1 mas modificada

--- Ramas/src/main/java/org/example/Main.java
+++ Ramas/src/main/java/org/example/Main.java
@@ -16,5 +16,6 @@ public class Main {
         //Estoy en la sagrada main... pues no estaba en la exp
         // modificando cosas en la otra rama
         //Ahora esta es una rama sobre la exp original
+        //seguimos modificando cosas sobre exp
     }
 }
