74f62d53dc47bde0f9647703f8344af4815d2d32
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-04-08 15:56:21 +0200

    mas cambios

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,16 @@ public class Main {
         }
         else if (ventagaParaGanar(puntosJugador1, puntosJugador2))
         {
-            return sacarVentagaParaGanarPuntuacion(puntosJugador1, puntosJugador2);
+            return sacarVentajaParaGanarPuntuacion(puntosJugador1, puntosJugador2);
         }
         else
         {
             return getRegularScore(puntosJugador1, puntosJugador2, "");
 
         }
+    }
+    private static boolean ventagaParaGanar(int puntosJugador1, int puntosJugador2) {
+        return puntosJugador1 >= 4 || puntosJugador2 >= 4;
     }
 
 private static String getRegularScore(int puntosJugador1, int puntosJugador2, String score) {
@@ -29,19 +32,14 @@ private static String obtenerPuntuacion(int puntos) {
     return (puntos >= 0 && puntos < puntuaciones.length) ? puntuaciones[puntos] : "";
 }
 
-    private static String sacarVentagaParaGanarPuntuacion(int puntosJugador1, int puntosJugador2) {
-        String score;
-        int minusResult = puntosJugador1 - puntosJugador2;
-        if (esEmpate(minusResult, 1)) score ="Advantage player1";
-        else if (esEmpate(minusResult, -1)) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
-        return score;
-    }
+private static String sacarVentajaParaGanarPuntuacion(int puntosJugador1, int puntosJugador2) {
+    int diferencia = puntosJugador1 - puntosJugador2;
+    if (diferencia == 1) return "Advantage player1";
+    if (diferencia == -1) return "Advantage player2";
+    if (diferencia >= 2) return "Win for player1";
+    return "Win for player2";
+}
 
-    private static boolean ventagaParaGanar(int puntosJugador1, int puntosJugador2) {
-        return puntosJugador1 >= 4 || puntosJugador2 >= 4;
-    }
 
 private static String extraerPuntuacionesDeEmpate(int puntosJugador1) {
     String[] puntuacionesDeEmpate = {"Love-All", "Fifteen-All", "Thirty-All", "Forty-All", "Deuce"};
